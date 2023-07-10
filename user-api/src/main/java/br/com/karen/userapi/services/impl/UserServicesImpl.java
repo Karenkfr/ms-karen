090df5f26574a6d8aac43ed5e2d5076e24843dce
Ribeiro <karen.ribeiro@capgemini.com>
2023-07-10 15:54:16 -0300

    load balancer commited

--- user-api/src/main/java/br/com/karen/userapi/services/impl/UserServicesImpl.java
+++ user-api/src/main/java/br/com/karen/userapi/services/impl/UserServicesImpl.java
@@ -3,6 +3,7 @@ package br.com.karen.userapi.services.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import br.com.karen.userapi.domain.User;
@@ -10,16 +11,19 @@ import br.com.karen.userapi.repositories.UserRepository;
 import br.com.karen.userapi.services.UserServices;
 import br.com.karen.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServicesImpl implements UserServices{
 
 	@Autowired
 	private UserRepository repository;
+	private final Environment env;
 
 	@Override
 	public User findById(Long id) {
+		log.info("USER_SERVICE :::  Get request on" +  env.getProperty("local.server.port" + "port"));
 		return repository.findById(id)
 				.orElseThrow(
 						() -> 
